6dc47e328c923644c5d103f0c1537da59d2773fd
Milton <miltonfragoso13@yahoo.com.br>
2022-05-31 00:32:24 -0300

    Implementado área trabalhe conosco

--- js/scripts.js
+++ js/scripts.js
@@ -101,9 +101,12 @@ $(document).ready(function() {
     })
 
     // Parallax 
+
+    // setTimeout serve para carregar primeiro as imagens
     setTimeout(function() {
       
         $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+        $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
 
     }, 250);
 
